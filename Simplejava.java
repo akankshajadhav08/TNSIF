6469fa3b7f3d6bdb9b8510f009ce26a80d18e42d
akankshajadhav08 <akanksha.jadhav8602@gmail.com>
2025-01-18 18:34:43 +0530

    Add files via upload

---
+++ Simplejava.java
@@ -0,0 +1,13 @@
+package day1TNSIF;
+
+public class Simplejava {
+	
+	public static void main(String args[])
+	              {
+		
+		System.out.println("Hello World");
+		
+		System.out.println("Hello Students");
+	              }
+
+}
