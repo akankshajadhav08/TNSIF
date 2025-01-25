36080f4ebcb568bceb8d9e065e6f4e20dd8b9809
akankshajadhav08 <akanksha.jadhav8602@gmail.com>
2025-01-25 11:19:03 +0530

    Add files via upload

---
+++ CharDemo.java
@@ -0,0 +1,20 @@
+package day2TNSIF;
+
+public class CharDemo {
+	
+	 public static void main(String[] args) {
+		 
+		 
+		 char ch='a';
+		 System.out.println(ch);
+		 
+		 
+		 char ch1=65;
+		 System.out.println(ch1);
+		 
+		 
+		 char vh1='\u00A7';
+		 System.out.println(vh1);
+	 }
+
+}
---
+++ breakdemo.java
@@ -0,0 +1,16 @@
+package day2TNSIF;
+
+public class breakdemo {
+	
+	   public static void main(String[] args) {
+		   
+		    for(int i=1; i<=10; i++)
+		    {
+		    	if(i==5)
+		    	{
+		    		break;
+		    	}
+		    	System.out.println(i);		    }
+	   }
+
+}
