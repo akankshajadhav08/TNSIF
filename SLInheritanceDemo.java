2e00f82832a541146e7dc39b675f6e29c8e6084a
akankshajadhav08 <akanksha.jadhav8602@gmail.com>
2025-01-31 21:18:40 +0530

    Add files via upload

---
+++ SLInheritanceDemo.java
@@ -0,0 +1,16 @@
+package day5TNSIF.SingleLevelinheritance;
+
+import day5TNSIF.Hierarchicalinheritance.Student;
+
+public class SLInheritanceDemo {
+
+		public static void main(String[] args) {
+		
+			Student st = new Student();
+			System.out.println(st);
+
+		}
+
+	}
+
+
